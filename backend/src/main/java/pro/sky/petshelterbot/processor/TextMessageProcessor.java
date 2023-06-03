a1f32fa1471198af18ec6199bcb6b524d375b8ca
Valeria <epif.vale@yandex.ru>
2023-06-03 14:27:32 +0300

    /info command JavaDoc

--- backend/src/main/java/pro/sky/petshelterbot/processor/TextMessageProcessor.java
+++ backend/src/main/java/pro/sky/petshelterbot/processor/TextMessageProcessor.java
@@ -96,6 +96,10 @@ public class TextMessageProcessor {
   // в соответствии с выбранным приютом (из таблицы last_commands)
   // + написать на свой метод javadoc
 
+  /**
+   * Отправляет информацию по выбранному пункту меню "Узнать информацию о приюте"
+   * @param lastCommand последняя команда пользователя
+   */
   private void processInfoCommand(LastCommand lastCommand) {
     if (lastCommand.getActiveType() == Type.CAT) {
       String message = ANSWERS.get("info_cat");
